02472d4abf80f8147336a92f7333cba077c8270a
Venky <venky@duh-uh.com>
2015-02-19 01:34:10 +0530

    [PAPI-778] Added support for "Visibility" RAML extension

--- docker/ramlparser.rb
+++ docker/ramlparser.rb
@@ -43,50 +43,63 @@ ramlfiles.each do |ramlfile|
 end
 abort 'No "Since:" tags found in any of the RAML files' if versions.empty?
 
-puts "Generating RAMLs for versions: #{versions.keys.sort}"
-versions.keys.sort.each do |version|
-  ramlfiles.each do |inputfile|
-    log "Processing file: #{inputfile}, version: #{version}" \
-      if version.length > 0
+[ 'public', 'private' ].each do |visibility|
+  visibility_filter = (visibility == 'public' ? 'public' : nil)
 
-    raml = RAML.new(inputfile)
-    raml.filternodes do |node, keys|
-      if (node.has_key?('description') and node['description'].is_a?(String))
-        since = node['description'].value_of('Since')
+  puts "\nGenerating #{visibility} RAMLs for versions: #{versions.keys.sort}"
+  versions.keys.sort.each do |version|
+    ramlfiles.each do |inputfile|
+      log "Processing file: #{inputfile}, version: #{version}"
 
-        # Drop nodes with a "since" value greater than specified version
-        since.greaterThanVersion(version)
+      raml = RAML.new(inputfile)
+      raml.filternodes do |node, keys|
+        if (node.has_key?('description') and node['description'].is_a?(String))
+          desc = node['description']
+          since = desc.value_of('Since')
+          vis = desc.value_of('Visibility').downcase
+
+          # Strip tokens from description
+          node['description'].gsub!(/\s*\[.*?\]\s*$/, '')
+
+          # Reinstate the "Since" token
+          node['description'] += " [Since:#{since}]" if since.length > 0
+
+          # Drop nodes with a "since" value greater than specified version
+          # or those with a visibility not matching specified value
+          since.greaterThanVersion(version) \
+            || (vis.length > 0 && visibility_filter && vis != visibility_filter)
+        end
       end
+
+      # Generate version-specific RAML file
+      outversdir = File.join(outputdir, visibility, version)
+      FileUtils.mkdir_p(outversdir) unless Dir.exists?(outversdir)
+
+      outvers = File.join(outversdir, File.basename(inputfile))
+      puts "Generating file: #{outvers}"
+      File.open(outvers, 'w') do |f|
+        f.write raml.dump
+      end
+
     end
-
-    # Generate version-specific RAML file
-    outversdir = File.join(outputdir, version)
-    Dir.mkdir(outversdir) unless Dir.exists?(outversdir)
-
-    outvers = File.join(outversdir, File.basename(inputfile))
-    puts "Generating file: #{outvers}"
-    File.open(outvers, 'w') do |f|
-      f.write raml.dump
-    end
-
   end
-end
 
-if (manifest)
-  abort "Manifest file not found: #{manifest}" unless File.exists?(manifest)
-  puts "\nCopying RAMLs into release directories"
-  releases = YAML.load(File.open(manifest))
-  releases.keys.each do |rel|
-    reldir = File.join(outputbase, 'releases', rel.to_s)
-    FileUtils.mkdir_p(reldir)
+  if (manifest)
+    abort "Manifest file not found: #{manifest}" unless File.exists?(manifest)
+    puts "\nCopying #{visibility} RAMLs into release directories"
+    releases = YAML.load(File.open(manifest))
+    releases.keys.each do |rel|
+      reldir = File.join(outputbase, 'releases', visibility, rel.to_s)
+      FileUtils.mkdir_p(reldir)
 
-    releases[rel].each do |r|
-      api = r.keys.first
-      vers = r[api]
-      infile = File.join(outputbase, 'versions', vers.to_s, api + '.raml')
-      outfile = File.join(reldir, api + '.raml')
-      puts "Creating #{outfile} (version #{vers})"
-      FileUtils.cp(infile, outfile)
+      releases[rel].each do |r|
+        api = r.keys.first
+        vers = r[api]
+        infile = File.join(outputbase, 'versions', visibility, vers.to_s, api + '.raml')
+        outfile = File.join(reldir, api + '.raml')
+        puts "Creating #{outfile} (version #{vers})"
+        FileUtils.cp(infile, outfile)
+      end
     end
   end
 end
